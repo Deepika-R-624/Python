64e4e618da233213ad3937303918a1516ac8a9c8
Deepika Raja <143886322+R-DEEPIKA-RAJA@users.noreply.github.com>
2023-10-15 19:04:31 +0530

    Day4

---
+++ 09PositiveNegativeOrZero.py
@@ -0,0 +1,15 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+num=float(input("enter the number"));
+if (num >0):
+   print("positive");
+elif (num<0):
+   print("negative");
+else:
+   print("zero");
